a4841cfaeeb1e16df16b3dc3210b3e7818364806
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-06-13 23:43:41 +0530

    Init the Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:hive/hive.dart';
 
 import './contact_page.dart';
 
-void main() => runApp(MyApp());
+void main()async{
+  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
