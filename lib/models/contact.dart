f61d7258dc6d212977ae2a42414485b923ce9179
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-06-15 00:22:58 +0530

    Problem solve of first do the changes and then perfom following command flutter packages pub run build_runner build

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,6 +1,17 @@
+import 'package:hive/hive.dart';
+
+part 'contact.g.dart';
+
+@HiveType()
 class Contact {
+
+  @HiveField(0)
   final String name;
+  @HiveField(1)
   final int age;
 
   Contact(this.name, this.age);
 }
+
+
+//to generate own adapter so that Hive can undertand the Contact data type use Hive Generator package
