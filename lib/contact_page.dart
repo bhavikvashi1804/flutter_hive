8dff22c2744d8a2066bbde81afcbe2db11051beb
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-06-15 01:07:06 +0530

    use WatchBoxBuilder to listen the updated data

--- lib/contact_page.dart
+++ lib/contact_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import './new_contact_form.dart';
 import './models/contact.dart';
@@ -23,20 +24,27 @@ class ContactPage extends StatelessWidget {
         ));
   }
 
-  ListView _buildListView() {
+  Widget _buildListView() {
 
-    final contactsBox=Hive.box('contacts'); 
-    
-    return ListView.builder(
-      itemCount: contactsBox.length,
-      itemBuilder: (context, index) {
-        final Contact oneContact=contactsBox.getAt(index) as Contact;
-        return ListTile(
-          title: Text(oneContact.name),
-          subtitle: Text(oneContact.age.toString()),
+    final contactsBox=Hive.box('contacts');
+
+    return WatchBoxBuilder(
+      box: contactsBox, 
+      builder: (context, box) {
+         return ListView.builder(
+          itemCount: contactsBox.length,
+          itemBuilder: (context, index) {
+            final Contact oneContact=contactsBox.getAt(index) as Contact;
+            return ListTile(
+              title: Text(oneContact.name),
+              subtitle: Text(oneContact.age.toString()),
+            );
+          } 
+        
         );
-      } 
-     
-    );
+      },
+    ); 
+    
+   
   }
 }
