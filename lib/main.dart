6d91242979ffd15fd02f1ad70ec914ca3ea038da
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-06-14 23:37:52 +0530

    Open Hive Box in init is problem

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,26 @@ void main()async{
   final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
   runApp(MyApp());
+
+  //first time you need to await and then next time you can use it no need to provide async
+  final contactsBox = await Hive.openBox('contacts');
+
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //after first time open you have  not to provide await to open box
+    //final contactsBox=Hive.openBox('contacts');
+    //but you have to close the box after the app is closed
+    //the better idea is to open the box per page
     return MaterialApp(
       title: 'Hive Tutorial',
-      home: ContactPage(),
+      home: FutureBuilder(
+
+        builder: (context, snapshot) => ContactPage(),
+        
+      ),
     );
   }
 }
